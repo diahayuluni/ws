45b54536399e96ed5bf604fe88a4b35a520ddf09
diahayuluni <diah.ayu.l@mail.ugm.ac.id>
2014-11-05 08:38:59 +0700

    add logout.php

--- xml/Login/logout.php
+++ xml/Login/logout.php
@@ -1,13 +1,10 @@
 <?php
-//memulai 
-session 
-session_s
-tart();
-//cek  adanya  session,  jika  session  ada  maka  akan  di  unset
-dan  dilanjutkan  dengan session_destroy
+//memulaisession
+session_start();
+//cek adanya session, jika session ada maka akan di unset dan dilanjutkan dengan session_destroy 
 if(ISSET($_SESSION['username'])){ 
-UNSET($_SESSION['username']);
+  UNSET($_SESSION['username']);
 }
-header("location: index.php");
+header("location: index.php"); 
 session_destroy();
 ?>
