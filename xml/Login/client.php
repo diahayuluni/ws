ecfd41e1ed4b163d93d1ed0dade447dadc7a03e1
diahayuluni <diah.ayu.l@mail.ugm.ac.id>
2014-11-05 08:41:39 +0700

    add client.php

--- xml/Login/client.php
+++ xml/Login/client.php
@@ -1,22 +1,21 @@
-<?php 
+<?php
 error_reporting(E_ALL);
-ini_set('dispalay_error',1);
+ini_set('display_error',1);
 session_start();
-
 //panggil library
 require_once('nusoap/lib/nusoap.php');
-//men
-$url = 'http://localhost/ws.git/xml/server.php?wsdl';
-
+//mendefinisikan alamat url service yang disediakan oleh client 
+$url = 'http://localhost/ws.git/xml/Login/server.php?wsdl';
+// $client = new soapclient($url); 
 $client = new nusoap_client($url, 'WSDL');
-
-$username = isset($_POST["username"]) ? $_POST["username"] : 'admin';
-$password = isset($_POST["password"]) ? $_POST["password"] : 'admin';
-$result = $client->call('login_ws',array('username'=>$username, 'password'=>$password));
-
-if($result == "Login Berhasil"){
-	$_SESSION['username']= $username;
+$username =  isset($_POST["username"]) ? $_POST["username"] : 'admin' ;
+$password =  isset($_POST["password"]) ? $_POST["password"] :  'admin' ;
+$result = $client->call('login_ws', array('username'=>$username, 'password'=>$password));
+// echo '<pre>';print_r($client->response);echo '</pre>';
+if($result == "Login Berhasil"){ 
+  $_SESSION['username'] = $username; 
+  header ("location:index.php");
 } else{
-		header ("location: login.php");
+  header ("location:login.php"); 
 }
 ?>
